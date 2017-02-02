92405e97fbc14fcb80949b79aa6de0f6f9c03403
sinmingu <alsrn556@naver.com>
2017-02-02 17:47:11 +0900

    modify QuizActivity.java

--- app/src/main/java/com/example/sinmingu/brailleproject/QuizActivity.java
+++ app/src/main/java/com/example/sinmingu/brailleproject/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.sinmingu.brailleproject;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class QuizActivity extends FragmentActivity {
 
@@ -10,5 +11,35 @@ public class QuizActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
+        TabHost tabHost = (TabHost)findViewById(R.id.quiz_tab_host);
+
+        tabHost.setup();
+
+        // Tab1 Setting
+        TabHost.TabSpec tabSpec1 = tabHost.newTabSpec("Tab1");
+        tabSpec1.setIndicator("초급"); // Tab Subject
+        tabSpec1.setContent(R.id.quiz_tab_view1); // Tab Content
+        tabHost.addTab(tabSpec1);
+
+        // Tab2 Setting
+        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("Tab2");
+        tabSpec2.setIndicator("중급"); // Tab Subject
+        tabSpec2.setContent(R.id.quiz_tab_view2); // Tab Content
+        tabHost.addTab(tabSpec2);
+
+
+        // Tab3 Setting
+        TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("Tab3");
+        tabSpec3.setIndicator("고급"); // Tab Subject
+        tabSpec3.setContent(R.id.quiz_tab_view3); // Tab Content
+        tabHost.addTab(tabSpec3);
+
+        // show First Tab Content
+        tabHost.setCurrentTab(0);
+
     }
+
+
 }
